876bf11c1cc51b0134f1ae8d7c1e20608685e1bf
Noah Dietz <noahdietz@ITs-MacBook-Air-15.local>
2015-06-25 14:42:31 -0700

    added supertest-pathNames basic tests

--- test/minimal/test.js
+++ test/minimal/test.js
@@ -4,8 +4,36 @@ var swagger = require('./swagger.json');
 
 describe('minimal swagger', function(){
 	describe('request-option', function(){
-		describe('pathNames-option empty', function(){
+		describe('pathNames-option', function(){
 			it ('should still generate all paths from empty pathNames option', function(done){
+        var output = testGen(swagger, {
+          'assertionFormat':'should',
+          'pathNames':[],
+          'testmodule':'request'
+        });
+
+        assert.isArray(output);
+        assert.lengthOf(output, 2);
+        done();
+      });
+
+      it ('should generate specified paths from pathNames option', function(done){
+        var output = testGen(swagger, {
+          'assertionFormat':'should',
+          'pathNames':['/'],
+          'testmodule':'request'
+        });
+
+        assert.isArray(output);
+        assert.lengthOf(output, 1);
+        done();
+      });
+		});
+	});
+
+	describe('supertest-option', function(){
+		describe('pathNames-option', function(){
+      it ('should still generate all paths from empty pathNames option', function(done){
         var output = testGen(swagger, {
           'assertionFormat':'should',
           'pathNames':[],
@@ -27,13 +55,7 @@ describe('minimal swagger', function(){
         assert.isArray(output);
         assert.lengthOf(output, 1);
         done();
-      })
-		});
-	});
-
-	describe('supertest option', function(){
-		describe('pathNames option', function(){
-
+      });
 		});
 	});
 });
