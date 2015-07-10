30057c419612f095aa3b6f9fe1f47aa1e031e3f0
Noah Dietz <noahdietz@ITs-MacBook-Air-7.local>
2015-07-10 15:36:53 -0700

    new compare files for new tests

---
+++ test/robust/compare/output13__Stub.js
@@ -0,0 +1,266 @@
+'use strict';
+var chai = require('chai');
+var ZSchema = require('z-schema');
+var validator = new ZSchema({});
+
+chai.should();
+var supertest = require('supertest');
+var api = supertest('https://api.uber.com'); // supertest init;
+
+describe('/', function() {
+  describe('get', function() {
+    it('should respond with 200 OK', function(done) {
+      api.get('/')
+      .set('Accept', 'application/json')
+      .expect(200)
+      .end(function(err, res) {
+        if (err) {
+          return done(err);
+        }
+
+        res.body.should.equal(null); // non-json response or no schema
+        done();
+      });
+    });
+    it('should respond with 200 OK', function(done) {
+      /*eslint-disable*/
+      var schema = {
+        "type": "object",
+        "properties": {
+          "id": {
+            "type": "integer"
+          },
+          "username": {
+            "type": "string"
+          }
+        }
+      };
+      /*eslint-enable*/
+      api.get('/')
+      .set('Accept', 'application/json')
+      .expect(200)
+      .end(function(err, res) {
+        if (err) {
+          return done(err);
+        }
+
+        validator.validate(res.body, schema).should.be.true;
+        done();
+      });
+    });
+    it('should respond with 400 NOT OK', function(done) {
+      api.get('/')
+      .set('Accept', 'application/json')
+      .expect(400)
+      .end(function(err, res) {
+        if (err) {
+          return done(err);
+        }
+
+        res.body.should.equal(null); // non-json response or no schema
+        done();
+      });
+    });
+    it('should respond with 400 NOT OK', function(done) {
+      /*eslint-disable*/
+      var schema = {
+        "type": "object",
+        "properties": {
+          "meta": "string",
+          "data": "number"
+        }
+      };
+      /*eslint-enable*/
+      api.get('/')
+      .set('Accept', 'application/json')
+      .expect(400)
+      .end(function(err, res) {
+        if (err) {
+          return done(err);
+        }
+
+        validator.validate(res.body, schema).should.be.true;
+        done();
+      });
+    });
+    it('should respond with 500 SERVER ERROR', function(done) {
+      api.get('/')
+      .set('Accept', 'application/json')
+      .expect(500)
+      .end(function(err, res) {
+        if (err) {
+          return done(err);
+        }
+
+        res.body.should.equal(null); // non-json response or no schema
+        done();
+      });
+    });
+    it('should respond with 500 SERVER ERROR', function(done) {
+      /*eslint-disable*/
+      var schema = {
+        "properties": {
+          "meta": "string",
+          "data": "number",
+          "UserObj": {
+            "schema": {
+              "type": "object",
+              "properties": {
+                "id": {
+                  "type": "integer"
+                },
+                "username": {
+                  "type": "string"
+                }
+              }
+            }
+          }
+        }
+      };
+      /*eslint-enable*/
+      api.get('/')
+      .set('Accept', 'application/json')
+      .expect(500)
+      .end(function(err, res) {
+        if (err) {
+          return done(err);
+        }
+
+        validator.validate(res.body, schema).should.be.true;
+        done();
+      });
+    });
+  });
+
+  describe('post', function() {
+    it('should respond with 200 OK', function(done) {
+      api.post('/?longitude=DATA')
+      .set('Accept', 'application/json')
+      .send({
+        latitude: 'DATA GOES HERE'
+      })
+      .expect(200)
+      .end(function(err, res) {
+        if (err) {
+          return done(err);
+        }
+
+        res.body.should.equal(null); // non-json response or no schema
+        done();
+      });
+    });
+
+    it('should respond with 200 OK', function(done) {
+      /*eslint-disable*/
+      var schema = {
+        "type": "array",
+        "items": {
+          "type": "object",
+          "properties": {
+            "id": {
+              "type": "integer"
+            },
+            "username": {
+              "type": "string"
+            }
+          }
+        }
+      };
+      /*eslint-enable*/
+      api.post('/?longitude=DATA')
+      .set('Accept', 'application/json')
+      .send({
+        latitude: 'DATA GOES HERE'
+      })
+      .expect(200)
+      .end(function(err, res) {
+        if (err) {
+          return done(err);
+        }
+
+        validator.validate(res.body, schema).should.be.true;
+        done();
+      });
+    });
+
+    it('should respond with 400 NOT OK', function(done) {
+      api.post('/?longitude=DATA')
+      .set('Accept', 'application/json')
+      .send({
+        latitude: 'DATA GOES HERE'
+      })
+      .expect(400)
+      .end(function(err, res) {
+        if (err) {
+          return done(err);
+        }
+
+        res.body.should.equal(null); // non-json response or no schema
+        done();
+      });
+    });
+
+    it('should respond with 400 NOT OK', function(done) {
+      /*eslint-disable*/
+      var schema = {
+        "type": "number"
+      };
+      /*eslint-enable*/
+      api.post('/?longitude=DATA')
+      .set('Accept', 'application/json')
+      .send({
+        latitude: 'DATA GOES HERE'
+      })
+      .expect(400)
+      .end(function(err, res) {
+        if (err) {
+          return done(err);
+        }
+
+        validator.validate(res.body, schema).should.be.true;
+        done();
+      });
+    });
+
+    it('should respond with 500 SERVER ERROR', function(done) {
+      api.post('/?longitude=DATA')
+      .set('Accept', 'application/json')
+      .send({
+        latitude: 'DATA GOES HERE'
+      })
+      .expect(500)
+      .end(function(err, res) {
+        if (err) {
+          return done(err);
+        }
+
+        res.body.should.equal(null); // non-json response or no schema
+        done();
+      });
+    });
+
+    it('should respond with 500 SERVER ERROR', function(done) {
+      /*eslint-disable*/
+      var schema = {
+        "type": "string"
+      };
+      /*eslint-enable*/
+      api.post('/?longitude=DATA')
+      .set('Accept', 'application/json')
+      .send({
+        latitude: 'DATA GOES HERE'
+      })
+      .expect(500)
+      .end(function(err, res) {
+        if (err) {
+          return done(err);
+        }
+
+        validator.validate(res.body, schema).should.be.true;
+        done();
+      });
+    });
+
+  });
+
+});
