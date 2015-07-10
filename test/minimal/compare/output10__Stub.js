a86ff1ffaaa166dc189bf8a5e962d2ad6269a1e6
Noah Dietz <noahdietz@ITs-MacBook-Air-7.local>
2015-07-10 15:36:53 -0700

    new compare files for new tests

---
+++ test/minimal/compare/output10__Stub.js
@@ -0,0 +1,38 @@
+'use strict';
+var chai = require('chai');
+
+chai.should();
+var supertest = require('supertest');
+var api = supertest('http://localhost:10010'); // supertest init;
+
+describe('/', function() {
+  describe('get', function() {
+    it('should respond with 200 OK', function(done) {
+      api.get('/')
+      .set('Accept', 'application/xml')
+      .expect(200)
+      .end(function(err, res) {
+        if (err) {
+          return done(err);
+        }
+
+        res.body.should.equal(null); // non-json response or no schema
+        done();
+      });
+    });
+    it('should respond with 200 OK', function(done) {
+      api.get('/')
+      .set('Accept', 'application/json')
+      .expect(200)
+      .end(function(err, res) {
+        if (err) {
+          return done(err);
+        }
+
+        res.body.should.equal(null); // non-json response or no schema
+        done();
+      });
+    });
+  });
+
+});
---
+++ test/robust/compare/output15_user_Stub.js
@@ -0,0 +1,530 @@
+'use strict';
+var chai = require('chai');
+
+chai.should();
+var request = require('request');
+
+describe('/user', function() {
+  describe('get', function() {
+    it('should respond with 200 OK', function(done) {
+      request({
+        url: 'https://api.uber.com/user',
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/xml'
+        }
+      },
+      function(error, res, body) {
+        if (error) {
+          return done(error);
+        }
+        res.statusCode.should.equal(200);
+
+        body.should.equal(null); // non-json response or no schema
+        done();
+      });
+    });
+
+    it('should respond with 200 OK', function(done) {
+      request({
+        url: 'https://api.uber.com/user',
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/xml'
+        }
+      },
+      function(error, res, body) {
+        if (error) {
+          return done(error);
+        }
+        res.statusCode.should.equal(200);
+
+        body.should.equal(null); // non-json response or no schema
+        done();
+      });
+    });
+
+    it('should respond with 200 OK', function(done) {
+      request({
+        url: 'https://api.uber.com/user',
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      },
+      function(error, res, body) {
+        if (error) {
+          return done(error);
+        }
+        res.statusCode.should.equal(200);
+
+        body.should.equal(null); // non-json response or no schema
+        done();
+      });
+    });
+
+    it('should respond with 200 OK', function(done) {
+      request({
+        url: 'https://api.uber.com/user',
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      },
+      function(error, res, body) {
+        if (error) {
+          return done(error);
+        }
+        res.statusCode.should.equal(200);
+
+        body.should.equal(null); // non-json response or no schema
+        done();
+      });
+    });
+
+    it('should respond with 400 NOT OK', function(done) {
+      request({
+        url: 'https://api.uber.com/user',
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/xml'
+        }
+      },
+      function(error, res, body) {
+        if (error) {
+          return done(error);
+        }
+        res.statusCode.should.equal(400);
+
+        body.should.equal(null); // non-json response or no schema
+        done();
+      });
+    });
+
+    it('should respond with 400 NOT OK', function(done) {
+      request({
+        url: 'https://api.uber.com/user',
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/xml'
+        }
+      },
+      function(error, res, body) {
+        if (error) {
+          return done(error);
+        }
+        res.statusCode.should.equal(400);
+
+        body.should.equal(null); // non-json response or no schema
+        done();
+      });
+    });
+
+    it('should respond with 400 NOT OK', function(done) {
+      request({
+        url: 'https://api.uber.com/user',
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      },
+      function(error, res, body) {
+        if (error) {
+          return done(error);
+        }
+        res.statusCode.should.equal(400);
+
+        body.should.equal(null); // non-json response or no schema
+        done();
+      });
+    });
+
+    it('should respond with 400 NOT OK', function(done) {
+      request({
+        url: 'https://api.uber.com/user',
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      },
+      function(error, res, body) {
+        if (error) {
+          return done(error);
+        }
+        res.statusCode.should.equal(400);
+
+        body.should.equal(null); // non-json response or no schema
+        done();
+      });
+    });
+
+    it('should respond with 500 SERVER ERROR', function(done) {
+      request({
+        url: 'https://api.uber.com/user',
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/xml'
+        }
+      },
+      function(error, res, body) {
+        if (error) {
+          return done(error);
+        }
+        res.statusCode.should.equal(500);
+
+        body.should.equal(null); // non-json response or no schema
+        done();
+      });
+    });
+
+    it('should respond with 500 SERVER ERROR', function(done) {
+      request({
+        url: 'https://api.uber.com/user',
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/xml'
+        }
+      },
+      function(error, res, body) {
+        if (error) {
+          return done(error);
+        }
+        res.statusCode.should.equal(500);
+
+        body.should.equal(null); // non-json response or no schema
+        done();
+      });
+    });
+
+    it('should respond with 500 SERVER ERROR', function(done) {
+      request({
+        url: 'https://api.uber.com/user',
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      },
+      function(error, res, body) {
+        if (error) {
+          return done(error);
+        }
+        res.statusCode.should.equal(500);
+
+        body.should.equal(null); // non-json response or no schema
+        done();
+      });
+    });
+
+    it('should respond with 500 SERVER ERROR', function(done) {
+      request({
+        url: 'https://api.uber.com/user',
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      },
+      function(error, res, body) {
+        if (error) {
+          return done(error);
+        }
+        res.statusCode.should.equal(500);
+
+        body.should.equal(null); // non-json response or no schema
+        done();
+      });
+    });
+
+  });
+
+  describe('post', function() {
+    it('should respond with 200 OK', function(done) {
+      request({
+        url: 'https://api.uber.com/user',
+        qs: {
+          longitude: 'DATA GOES HERE'
+        },
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/xml'
+        },
+        body: 'XML STRING GOES HERE'
+      },
+      function(error, res, body) {
+        if (error) {
+          return done(error);
+        }
+        res.statusCode.should.equal(200);
+
+        body.should.equal(null); // non-json response or no schema
+        done();
+      });
+    });
+
+    it('should respond with 200 OK', function(done) {
+      request({
+        url: 'https://api.uber.com/user',
+        qs: {
+          longitude: 'DATA GOES HERE'
+        },
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/xml'
+        },
+        body: 'XML STRING GOES HERE'
+      },
+      function(error, res, body) {
+        if (error) {
+          return done(error);
+        }
+        res.statusCode.should.equal(200);
+
+        body.should.equal(null); // non-json response or no schema
+        done();
+      });
+    });
+
+    it('should respond with 200 OK', function(done) {
+      request({
+        url: 'https://api.uber.com/user',
+        qs: {
+          longitude: 'DATA GOES HERE'
+        },
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        json: {
+          latitude: 'DATA GOES HERE'
+        }
+      },
+      function(error, res, body) {
+        if (error) {
+          return done(error);
+        }
+        res.statusCode.should.equal(200);
+
+        body.should.equal(null); // non-json response or no schema
+        done();
+      });
+    });
+
+    it('should respond with 200 OK', function(done) {
+      request({
+        url: 'https://api.uber.com/user',
+        qs: {
+          longitude: 'DATA GOES HERE'
+        },
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        json: {
+          latitude: 'DATA GOES HERE'
+        }
+      },
+      function(error, res, body) {
+        if (error) {
+          return done(error);
+        }
+        res.statusCode.should.equal(200);
+
+        body.should.equal(null); // non-json response or no schema
+        done();
+      });
+    });
+
+    it('should respond with 400 NOT OK', function(done) {
+      request({
+        url: 'https://api.uber.com/user',
+        qs: {
+          longitude: 'DATA GOES HERE'
+        },
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/xml'
+        },
+        body: 'XML STRING GOES HERE'
+      },
+      function(error, res, body) {
+        if (error) {
+          return done(error);
+        }
+        res.statusCode.should.equal(400);
+
+        body.should.equal(null); // non-json response or no schema
+        done();
+      });
+    });
+
+    it('should respond with 400 NOT OK', function(done) {
+      request({
+        url: 'https://api.uber.com/user',
+        qs: {
+          longitude: 'DATA GOES HERE'
+        },
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/xml'
+        },
+        body: 'XML STRING GOES HERE'
+      },
+      function(error, res, body) {
+        if (error) {
+          return done(error);
+        }
+        res.statusCode.should.equal(400);
+
+        body.should.equal(null); // non-json response or no schema
+        done();
+      });
+    });
+
+    it('should respond with 400 NOT OK', function(done) {
+      request({
+        url: 'https://api.uber.com/user',
+        qs: {
+          longitude: 'DATA GOES HERE'
+        },
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        json: {
+          latitude: 'DATA GOES HERE'
+        }
+      },
+      function(error, res, body) {
+        if (error) {
+          return done(error);
+        }
+        res.statusCode.should.equal(400);
+
+        body.should.equal(null); // non-json response or no schema
+        done();
+      });
+    });
+
+    it('should respond with 400 NOT OK', function(done) {
+      request({
+        url: 'https://api.uber.com/user',
+        qs: {
+          longitude: 'DATA GOES HERE'
+        },
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        json: {
+          latitude: 'DATA GOES HERE'
+        }
+      },
+      function(error, res, body) {
+        if (error) {
+          return done(error);
+        }
+        res.statusCode.should.equal(400);
+
+        body.should.equal(null); // non-json response or no schema
+        done();
+      });
+    });
+
+    it('should respond with 500 SERVER ERROR', function(done) {
+      request({
+        url: 'https://api.uber.com/user',
+        qs: {
+          longitude: 'DATA GOES HERE'
+        },
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/xml'
+        },
+        body: 'XML STRING GOES HERE'
+      },
+      function(error, res, body) {
+        if (error) {
+          return done(error);
+        }
+        res.statusCode.should.equal(500);
+
+        body.should.equal(null); // non-json response or no schema
+        done();
+      });
+    });
+
+    it('should respond with 500 SERVER ERROR', function(done) {
+      request({
+        url: 'https://api.uber.com/user',
+        qs: {
+          longitude: 'DATA GOES HERE'
+        },
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/xml'
+        },
+        body: 'XML STRING GOES HERE'
+      },
+      function(error, res, body) {
+        if (error) {
+          return done(error);
+        }
+        res.statusCode.should.equal(500);
+
+        body.should.equal(null); // non-json response or no schema
+        done();
+      });
+    });
+
+    it('should respond with 500 SERVER ERROR', function(done) {
+      request({
+        url: 'https://api.uber.com/user',
+        qs: {
+          longitude: 'DATA GOES HERE'
+        },
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        json: {
+          latitude: 'DATA GOES HERE'
+        }
+      },
+      function(error, res, body) {
+        if (error) {
+          return done(error);
+        }
+        res.statusCode.should.equal(500);
+
+        body.should.equal(null); // non-json response or no schema
+        done();
+      });
+    });
+
+    it('should respond with 500 SERVER ERROR', function(done) {
+      request({
+        url: 'https://api.uber.com/user',
+        qs: {
+          longitude: 'DATA GOES HERE'
+        },
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        json: {
+          latitude: 'DATA GOES HERE'
+        }
+      },
+      function(error, res, body) {
+        if (error) {
+          return done(error);
+        }
+        res.statusCode.should.equal(500);
+
+        body.should.equal(null); // non-json response or no schema
+        done();
+      });
+    });
+
+  });
+
+});
