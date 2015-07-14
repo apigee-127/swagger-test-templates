374b6d6eea280d5bc2c981dc609dc85da60e92fb
Noah Dietz <noahdietz@ITs-MacBook-Air-7.local>
2015-07-13 17:57:44 -0700

    add basic test cases for pathify helper

---
+++ test/pathParam/compare/output1-{id}-test.js
@@ -0,0 +1,48 @@
+'use strict';
+var chai = require('chai');
+
+chai.should();
+var request = require('request');
+
+describe('/{id}', function() {
+  describe('get', function() {
+    it('should respond with 200 OK', function(done) {
+      request({
+        url: 'http://localhost:10010/{PARAM GOES HERE}',
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      },
+      function(error, res, body) {
+        if (error) return done(error);
+
+        res.statusCode.should.equal(200);
+
+        body.should.equal(null); // non-json response or no schema
+        done();
+      });
+    });
+
+  });
+
+  describe('head', function() {
+    it('should respond with 200 OK', function(done) {
+      request({
+        url: 'http://localhost:10010/{PARAM GOES HERE}',
+        method: 'HEAD',
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      },
+      function(error, res) {
+        if (error) return done(error);
+
+        res.statusCode.should.equal(200);
+        done();
+      });
+    });
+
+  });
+
+});
