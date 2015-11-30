9dc1576bc9586653a3a8f662ae36aeadef219109
Max Mueller <maxmueller@eaton.com>
2015-11-30 11:58:02 -0500

    Add support for path parameters to handlebars `pathify`
    - A few quick tests seem to indicate that this works fine
    - Was designed to replace multiple parameters, but needs to be tested
    further
    - Could probably be optimized down to a few lines, but this was
    quick'n'dirty :)
    - No error checking on value of pathParam -- should probably only be
    allowed to be javascript native
    - Only works globally -- I wonder if there is a way to make this easier
    to do in a per-path fashion (right now, selfishly, it serves the purpose
    I need it for)
    
    Updated to make path parameter replacement use regex for one to many sets of curly braces
    
    - Now, path parameters will be replaced in the form of {param} and/or
    {{param}} (or however many curly braces are used)
    - It should also match on sets of uneven curly braces, such as {param}},
    this may be undesirable behavior
    
    Added test case for functionality
    
    - Added unit test in test/pathParam/test.js
    - Added diff file test/pathParam/compare/output3-{id}-test.js
    
    Replace windows newline with unix newline for test
    
    - Developing on windows, so created diff file had \r\n as newline,
    whereas generated code just uses \n
    - Added .replace(/\r\n/g, '\n') on read
    - There should be a note made in the readme about making sure to do this
    (probably across all tests)

---
+++ test/pathParam/compare/output3-{id}-test.js
@@ -0,0 +1,37 @@
+'use strict';
+var chai = require('chai');
+var supertest = require('supertest');
+var api = supertest('http://localhost:10010'); // supertest init;
+
+chai.should();
+
+describe('/{id}', function() {
+  describe('get', function() {
+    it('should respond with 200 OK', function(done) {
+      api.get('/userSuppliedID')
+      .set('Accept', 'application/json')
+      .expect(200)
+      .end(function(err, res) {
+        if (err) return done(err);
+
+        res.body.should.equal(null); // non-json response or no schema
+        done();
+      });
+    });
+
+  });
+
+  describe('head', function() {
+    it('should respond with 200 OK', function(done) {
+      api.head('/userSuppliedID')
+      .set('Accept', 'application/json')
+      .expect(200)
+      .end(function(err) {
+        if (err) return done(err);
+        done();
+      });
+    });
+
+  });
+
+});
