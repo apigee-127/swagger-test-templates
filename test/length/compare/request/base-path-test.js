5cd1156287155b9436dd9deab290d06f9c5a3a72
Noah Dietz <noahdietz24@gmail.com>
2016-09-05 13:32:55 -0700

    add toggle for description truncation

---
+++ test/length/compare/request/base-path-test.js
@@ -0,0 +1,29 @@
+'use strict';
+var chai = require('chai');
+var request = require('request');
+
+chai.should();
+
+describe('/', function() {
+  describe('get', function() {
+    it('should respond with 200 OK, and this description is extra long for a simple length toggle test!', function(done) {
+      request({
+        url: 'http://basic.herokuapp.com/',
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      },
+      function(error, res, body) {
+        if (error) return done(error);
+
+        res.statusCode.should.equal(200);
+
+        body.should.equal(null); // non-json response or no schema
+        done();
+      });
+    });
+
+  });
+
+});
