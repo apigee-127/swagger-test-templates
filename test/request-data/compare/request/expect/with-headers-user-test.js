eb89a87e5c363086146fd577a23b56c3d6d00346
Simon Baynes <baynezy@gmail.com>
2017-05-09 02:11:14 +0100

    Update requestParams to work with HTTP headers (#142)
    * Replace HTTP header data with requestData
    
    Request data will now populate headers
    
    * Update tests to reflect latest changes in master
    
    * Improve the documentation around using requestData.

---
+++ test/request-data/compare/request/expect/with-headers-user-test.js
@@ -0,0 +1,73 @@
+'use strict';
+var chai = require('chai');
+var request = require('request');
+var expect = chai.expect;
+
+describe('/user', function() {
+  describe('get', function() {
+    it('should respond with 200 OK and some description', function(done) {
+      request({
+        url: 'https://api.uber.com/user',
+        json: true,
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          'X-Longitude': 'DATA GOES HERE',
+          'X-Token': 'sadfg'
+        }
+      },
+      function(error, res, body) {
+        if (error) return done(error);
+
+        expect(res.statusCode).to.equal(200);
+
+        expect(body).to.equal(null); // non-json response or no schema
+        done();
+      });
+    });
+
+    it('should respond with 400 NOT OK', function(done) {
+      request({
+        url: 'https://api.uber.com/user',
+        json: true,
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          'X-Longitude': 'DATA GOES HERE',
+          'X-Token': 'DATA GOES HERE'
+        }
+      },
+      function(error, res, body) {
+        if (error) return done(error);
+
+        expect(res.statusCode).to.equal(400);
+
+        expect(body).to.equal(null); // non-json response or no schema
+        done();
+      });
+    });
+
+    it('should respond with 500 SERVER ERROR', function(done) {
+      request({
+        url: 'https://api.uber.com/user',
+        json: true,
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          'X-Longitude': 'DATA GOES HERE',
+          'X-Token': 'DATA GOES HERE'
+        }
+      },
+      function(error, res, body) {
+        if (error) return done(error);
+
+        expect(res.statusCode).to.equal(500);
+
+        expect(body).to.equal(null); // non-json response or no schema
+        done();
+      });
+    });
+
+  });
+
+});
