d0e8c0ae352ef1d4ef178eb7dc34f19f2b6c4e28
Simon Baynes <simon.baynes@experian.com>
2017-05-11 14:00:34 +0100

    Fix issue where non-standard media types were not showing requestData
    Previously, perfectly valid media types that are JSON but not an
    exact match for 'application/json' e.g 'application/json; charset=charset=utf-8'
    were causing requestData not to show

---
+++ test/request-data/compare/request/expect/non-standard-c-type-products-test.js
@@ -0,0 +1,30 @@
+'use strict';
+var chai = require('chai');
+var request = require('request');
+var expect = chai.expect;
+
+describe('/products', function() {
+  describe('post', function() {
+    it('should respond with 200 OK and some description', function(done) {
+      request({
+        url: 'https://api.uber.com/products',
+        json: true,
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/vnd:something+json'
+        },
+        body: {}
+      },
+      function(error, res, body) {
+        if (error) return done(error);
+
+        expect(res.statusCode).to.equal(200);
+
+        expect(body).to.equal(null); // non-json response or no schema
+        done();
+      });
+    });
+
+  });
+
+});
--- test/request-data/test.js
+++ test/request-data/test.js
@@ -1,27 +1,3 @@
-/*
- * The MIT License (MIT)
- *
- * Copyright (c) 2015 Apigee Corporation
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
-
 'use strict';
 
 var assert = require('chai').assert;
@@ -30,6 +6,7 @@ var swaggerPost = require('./swagger-post.json');
 var swaggerGet = require('./swagger-get.json');
 var swaggerGet2 = require('./swagger-get2.json');
 var swaggerGetWithHeaders = require('./swagger-get-with-headers.json');
+var swaggerNonStandardContentType = require('./swagger-with-non-standard-content-type.json');
 var yaml = require('js-yaml');
 var join = require('path').join;
 var rules;
@@ -69,6 +46,71 @@ describe('request data population', function() {
 
           assert.isArray(output1);
           assert.lengthOf(output1, 1);
+
+          var generatedCode;
+
+          for (ndx in paths1) {
+            if (paths1 !== undefined) {
+              generatedCode = read(paths1[ndx], 'utf8').replace(/\r\n/g, '\n');
+              assert.equal(output1[ndx].test.replace(/\r\n/g, '\n'), generatedCode);
+            }
+          }
+        });
+      });
+    });
+
+    describe('with non-standard content type', function() {
+      describe('expect', function() {
+        var output1 = testGen(swaggerNonStandardContentType, {
+          assertionFormat: 'expect',
+          pathName: [],
+          testModule: 'request',
+          maxLen: -1,
+          requestData: {
+            '/products': {
+              post: {
+                200: [
+                  {
+                    body: {},
+                    description: 'some description'
+                  }
+                ]
+              }
+            },
+            '/products/{id}': {
+              get: {
+                200: [
+                  {
+                    id: 2,
+                    description: 'some description'
+                  }
+                ]
+              },
+              put: {
+                200: [
+                  {
+                    id: 2,
+                    body: {},
+                    description: 'some description'
+                  }
+                ]
+              }
+            }
+          }
+        });
+
+        var paths1 = [];
+        var ndx;
+
+        for (ndx in output1) {
+          if (output1) {
+            paths1.push(join(__dirname, '/compare/request/expect/non-standard-c-type-' + output1[ndx].name));
+          }
+        }
+
+        it('should populate request data', function() {
+          assert.isArray(output1);
+          assert.lengthOf(output1, 2);
 
           var generatedCode;
 
